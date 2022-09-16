7f239cd1a9f7a54913d734f90fffeed15590b68d
MinJun <68832065+wkdalswns0427@users.noreply.github.com>
2022-09-16 12:57:51 +0900

    Update pico1_main.py

--- TEST/PICO/pico1_main.py
+++ TEST/PICO/pico1_main.py
@@ -1,11 +1,49 @@
-from pico1_functions import function1, function2
+import time
+import os
+import machine
+uart = machine.UART(0, 115200)
+sens = 10
+secs = 10
+
+def function1(endtime):
+    print('fun1 init')
+    init = time.time()
+    while True:
+        if (time.time()-init) > endtime:
+            break
+        for i in range(3):
+            if (time.time()-init) > endtime:
+                break
+            uart.write('1'.encode('utf-8'))
+            time.sleep(1)
+        if (time.time()-init) > endtime:
+            break
+        uart.write('0'.encode('utf-8'))
+        time.sleep(1)
+
+def function2(endtime):
+    num = 50
+    init = time.time()
+    while num<=100:
+        if (time.time()-init) > endtime:
+            break
+        uart.write(str(num).encode('utf-8'))
+        time.sleep(2)
+        num += 2
 
 def main():
-    selection = int(input("choose sensor : "))
-    selecttime = int(input("config time : "))
-    if selection == 1:
-        function1(selecttime)
-    elif selection == 2:
-        function2(selecttime)
-        
+    sens, secs = None, None
+    while True:
+        if sens == None and uart.any():
+            sens = int(uart.readline().decode('utf-8'))
+            time.sleep(3)
+            secs = int(uart.readline().decode('utf-8'))
+            
+        if sens == 1:
+            function1(secs)
+            break
+        elif sens == 2:
+            function2(secs)
+            break
+            
 main()
